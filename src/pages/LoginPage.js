c81cfe7161c2234756c1d73503519154ef8194cd
Ayush Ranjan Sinha <asayushranjansinha.com>
2023-06-26 18:31:10 +0530

    react router set up and login and sign up added

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Login from "../features/auth/Login";
+const LoginPage = () => {
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  );
+};
+
+export default LoginPage;
