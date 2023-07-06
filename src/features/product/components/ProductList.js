6a33d91b0b6303de78e108b84202948b51fc8630
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2023-07-06 16:31:43 +0530

    pagination shifted to a com

--- src/features/product/components/ProductList.js
+++ src/features/product/components/ProductList.js
@@ -9,7 +9,6 @@ import {
   PlusIcon,
   Squares2X2Icon,
 } from "@heroicons/react/20/solid";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -22,6 +21,7 @@ import {
   fetchCategoriesAsync,
 } from "../productSlice";
 import { ITEMS_PER_PAGE } from "../../../app/constants";
+import Pagination from "../../shared/Pagination";
 const sortOptions = [
   { name: "Best Rating", sort: "rating", order: "desc", current: false },
   { name: "Price: Low to High", sort: "price", order: "asc", current: false },
@@ -50,7 +50,7 @@ const ProductList = () => {
   const [filter, setFilter] = useState({});
   const [sort, setSort] = useState({});
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
-  const [page, setPage] = useState(1);
+  const [pageno, setPage] = useState(1);
 
   // Filters for the product list
   const filters = [
@@ -92,18 +92,18 @@ const ProductList = () => {
     setSort(sort);
   };
 
-  // handlePage function to take next page as input and set the page state
-  const handlePage = (page) => {
-    setPage(page);
+  // handlePage function to take next pageno as input and set the pageno state
+  const handlePage = (pageno) => {
+    setPage(pageno);
   };
 
-  // useEffect to render on page,filter,dispatch,sort changes
+  // useEffect to render on pageno,filter,dispatch,sort changes
   useEffect(() => {
-    const pagination = { _page: page, limit: ITEMS_PER_PAGE };
+    const pagination = { _page: pageno, limit: ITEMS_PER_PAGE };
     dispatch(fetchProductsByFiltersAsync({ filter, sort, pagination }));
-  }, [dispatch, filter, sort, page]);
+  }, [dispatch, filter, sort, pageno]);
 
-  // useEffect to set page = 1 on every sort
+  // useEffect to set pageno = 1 on every sort
   useEffect(() => {
     setPage(1);
   }, [totalItems, sort]);
@@ -209,8 +209,7 @@ const ProductList = () => {
 
           {/* Pagination */}
           <Pagination
-            page={page}
-            setPage={setPage}
+            pageno={pageno}
             handlePage={handlePage}
             totalItems={totalItems}
           />
@@ -417,99 +416,6 @@ function ProductGrid({ products }) {
               </Link>
             ))}
           </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-// PAGINATION COMPONENT
-function Pagination({ handlePage, setpage, page, totalItems }) {
-  const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
-  return (
-    <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-      <div className="flex flex-1 justify-between sm:hidden">
-        <div
-          onClick={(e) => {
-            if (page > 1) {
-              handlePage(page - 1);
-            }
-          }}
-          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-        >
-          Previous
-        </div>
-        <div
-          onClick={(e) => {
-            if (page + 1 <= totalPages) {
-              handlePage(page + 1);
-            }
-          }}
-          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-        >
-          Next
-        </div>
-      </div>
-      <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-        <div>
-          <p className="text-sm text-gray-700">
-            Showing{" "}
-            <span className="font-medium">
-              {(page - 1) * ITEMS_PER_PAGE + 1}
-            </span>{" "}
-            to{" "}
-            <span className="font-medium">
-              {page * ITEMS_PER_PAGE > totalItems
-                ? totalItems
-                : page * ITEMS_PER_PAGE}
-            </span>{" "}
-            of <span className="font-medium">{totalItems}</span> results
-          </p>
-        </div>
-        <div>
-          <nav
-            className="isolate inline-flex -space-x-px rounded-md shadow-sm"
-            aria-label="Pagination"
-          >
-            <div
-              onClick={(e) => {
-                if (page > 1) {
-                  handlePage(page - 1);
-                }
-              }}
-              className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
-              <span className="sr-only">Previous</span>
-              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-            </div>
-            {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-
-            {Array.from({ length: totalPages }).map((el, index) => (
-              <div
-                key={index}
-                onClick={(e) => handlePage(index + 1)}
-                aria-current="page"
-                className={`relative cursor-pointer z-10 inline-flex items-center px-4 py-2 text-sm font-semibold  focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${
-                  index + 1 === page
-                    ? "bg-indigo-600 text-white"
-                    : "text-gray-400"
-                }`}
-              >
-                {index + 1}
-              </div>
-            ))}
-            <button
-              onClick={(e) => {
-                if (page + 1 <= totalPages) {
-                  handlePage(page + 1);
-                }
-              }}
-              className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
-              <span className="sr-only">Next</span>
-              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-            </button>
-          </nav>
         </div>
       </div>
     </div>
