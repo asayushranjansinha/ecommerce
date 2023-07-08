969ff96e4a8198488327e64050544a2991b53f8e
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2023-07-08 10:40:34 +0530

    a slight bug removed in ui

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -6,18 +6,19 @@ import {
 } from "./cartSlice";
 import { Link } from "react-router-dom";
 import { Navigate } from "react-router-dom";
-import { discountedPrice } from "../../app/constants";
+import { discountedPrice } from '../../app/constants';
 
 export default function Cart() {
   const dispatch = useDispatch();
 
   const items = useSelector(selectItems);
   const totalAmount = items.reduce(
+    (amount, item) => item.price * item.quantity + amount,
     (amount, item) => discountedPrice(item) * item.quantity + amount,
     0
   );
-  console.log()
   const totalItems = items.reduce((total, item) => item.quantity + total, 0);
+
   const handleQuantity = (e, item) => {
     dispatch(updateCartAsync({ ...item, quantity: +e.target.value }));
   };
@@ -54,7 +55,6 @@ export default function Cart() {
                           <h3>
                             <a href={item.href}>{item.title}</a>
                           </h3>
-                          <p className="ml-4">${item.price}</p>
                           <p className="ml-4">${discountedPrice(item)}</p>
                         </div>
                         <p className="mt-1 text-sm text-gray-500">
